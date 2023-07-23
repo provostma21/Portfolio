d25510f5097d55b8332ea1ccfe29316ebc71aedf
Matthew M. Provost <provostmat@gmail.com>
2023-07-22 22:33:44 -0400

    updated Home.js body

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,20 @@ export default function Home() {
     <>
       <header>Matthew M. Provost</header>
       <h2>About Me</h2>
-      <p>Aaaaaaahhhhh</p>
+      <p>
+        Hello there and thank you for taking the time to visit my portfolio! My
+        name is Matthew Provost. I was born in New Haven, Connecticut and have
+        lived in Connecticut for practically all of my life. My interest in
+        computers sparked years ago when I saw the MMORPG World of Warcraft.
+        After that I switched from console to PC gaming which then lead to me
+        modifying my first computer followed by my first PC build. After that I
+        became more interested in the internal functions of computers. I started
+        to spend time looking into coding and then joined a bootcamp affiliated
+        with Columbia University to become a Full-Stack software developer. I
+        have much to learn but I'm also very excited about the journey I am on.
+        Feel free to take a look at my work and let me know what you think.
+        Thank you very much for your time!
+      </p>
     </>
   );
 }
