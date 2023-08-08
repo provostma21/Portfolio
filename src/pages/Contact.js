f13e10fc55e79d2b655c4ccb7e3195d39423ecb5
Matthew M. Provost <provostmat@gmail.com>
2023-08-07 20:41:34 -0400

    edited GitHub contact info

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,7 +2,7 @@ export default function Contact() {
   return (
     <>
       <h3>Email: provostmat@gmail.com</h3>
-      <h3>GitHub:</h3>
+      <h3>GitHub: provostma21</h3>
       <h3>LinkedIn:</h3>
     </>
   );
