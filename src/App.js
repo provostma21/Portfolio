13fcb05e63e336ee40d29c31a1e2b83a2f9f2cc0
Matthew M. Provost <provostmat@gmail.com>
2023-08-10 21:28:49 -0400

    added Projects component to app.js file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Home from "../src/pages/Home";
+import Projects from "../src/pages/Projects";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Home />
+        <Projects />
       </header>
     </div>
   );
