c900bb1ccc6df648b8625568972bc126d369c782
Matthew M. Provost <provostmat@gmail.com>
2023-08-13 20:53:57 -0400

    modified Project.js page

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,7 +1,9 @@
 import projects from "../assets/projects/ProjectList";
 
 export default function Project() {
-  const listItems = projects.map((project) => <li key={project.id}></li>);
+  const listItems = projects.map((project) => (
+    <li key={project.id}>{project.name}</li>
+  ));
 
   return <ul>{listItems}</ul>;
 }
