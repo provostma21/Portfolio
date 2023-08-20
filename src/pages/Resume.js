fec34f77ce3a65ce8959632fbf0ee459c59d6ff0
Matthew M. Provost <provostmat@gmail.com>
2023-08-19 21:02:00 -0400

    added content to resume.js to allow resume download

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,3 +1,32 @@
+import React from "react";
+import Resume from "../../assets/images/grinning_wolf.jpg";
+
 export default function Resume() {
-  return;
+  return (
+    <div>
+      <h1>Resume</h1>
+      <h2>Skills</h2>
+      <ul className="skills">
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+        <li>Express</li>
+        <li>NodeJS</li>
+        <li>RESTful APIs</li>
+        <li>Mongoose</li>
+        <li>ReactJS</li>
+      </ul>
+      <p>
+        I have graduated from the Part-time Full-Stack Coding Boot Camp with
+        Columbia University and am currently engaging in self-study to improve
+        my coding skills. If you are interested, please feel free to download my
+        resume below!
+      </p>
+      <div className="resume">
+        <a href={Resume} download>
+          Download Here!{" "}
+        </a>
+      </div>
+    </div>
+  );
 }
