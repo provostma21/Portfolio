d7682f0f84254588f77a0379d2c3032d9133fb37
Matthew M. Provost <provostmat@gmail.com>
2023-09-12 16:04:14 -0400

    reworked projectData.map function to now display name, image, link, and repolink

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -26,7 +26,25 @@ import { projectData } from "./ProjectList";
 
 function Project(props) {
   const arrayDataItems = projectData.map((projectData) => (
-    <li>{projectData.name}</li>
+    <li className="project-list-item" key={projectData.id}>
+      {`${projectData.name}`}{" "}
+      {
+        <img
+          key={projectData.id}
+          src={projectData.image}
+          alt={projectData.name}
+        />
+      }{" "}
+      `${projectData.link} ${projectData.repoLink}`}
+    </li>
+    // (<li>{projectData.name}</li>),
+    // (
+    //   <img
+    //     key={projectData.id}
+    //     src={projectData.image}
+    //     alt={projectData.name}
+    //   />
+    // )
   ));
   return (
     <div className="container">
