84f54edb7bb69e06e53135a84587cf0840c42f60
Matthew M. Provost <provostmat@gmail.com>
2023-09-16 20:56:02 -0400

    added Navbar function to Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,46 @@
 import "./Style/Navbar.css";
 
-export default function Navbar() {
-  return;
+export default function Navbar({ currentPage, handlePageChange }) {
+  return (
+    <ul className="nav navbar">
+      <li className="nav-item">
+        <a
+          href="#home"
+          onClick={() => handlePageChange("Home")}
+          className={currentPage === "Home" ? "nav-link active" : "nav-link"}
+        >
+          Home
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#projects"
+          onClick={() => handlePageChange("Projects")}
+          className={
+            currentPage === "Projects" ? "nav-link active" : "nav-link"
+          }
+        >
+          Projects
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#resume"
+          onClick={() => handlePageChange("Resume")}
+          className={currentPage === "Resume" ? "nav-link active" : "nav-link"}
+        >
+          Resume
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#contact"
+          onClick={() => handlePageChange("Contact")}
+          className={currentPage === "Contact" ? "nav-link active" : "nav-link"}
+        >
+          Contact
+        </a>
+      </li>
+    </ul>
+  );
 }
