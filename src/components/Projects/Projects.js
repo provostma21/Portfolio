59f7a344e1e9edd6c5af5ccc3eb762c1908b4a43
Matthew M. Provost <provostmat@gmail.com>
2023-09-19 22:11:21 -0400

    removed commented out code from Project.js for displaying the projectData

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,35 +2,6 @@
 import "../Style/Projects.css";
 import { projectData } from "./ProjectList";
 
-// const styles = {
-//   image: {
-//     height: "200px",
-//     length: "200px",
-//   },
-// };
-// function Project({ projects }) {
-//   return (
-//     <>
-//       <th>{projects}</th>
-//     </>
-//   );
-// }
-
-// export default function ProjectGrid({ projects }) {
-//   const cards = [];
-//   let lastProject = null;
-
-//   projects.forEach((project) => {
-//     if (project.id !== lastProject) {
-//       cards.push(<Project id={project.id} key={project.id} />);
-//     }
-//     cards.push(<ProjectCard project={project} key={project.id} />);
-//     lastProject = project.id;
-//   });
-
-//   return <grid>{cards}</grid>;
-// }
-
 function Project(props) {
   const arrayDataItems = projectData.map((projectData) => (
     <li className="project-list-item" key={projectData.id}>
