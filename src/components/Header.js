e684068affa68536e12333aab035ce08faf36e22
Matthew M. Provost <provostmat@gmail.com>
2023-09-21 18:52:59 -0400

    added some coloration styling to header and removed padding/margins of header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,10 @@
+import "./Style/Header.css";
+
 export default function Header() {
   return (
-    <>
+    <div className="header-container">
       <header>Matthew M. Provost</header>
       <h2>Software Developer</h2>
-    </>
+    </div>
   );
 }
