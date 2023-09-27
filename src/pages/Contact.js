9ae6e8515b8185448046c9f07d72e5efb3d3cb49
Matthew M. Provost <provostmat@gmail.com>
2023-09-26 23:09:13 -0400

    removed email contact from contact.js

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,9 +2,5 @@ import React from "react";
 import "../pages/Styles/Contact.css";
 
 export default function Contact() {
-  return (
-    <>
-      <h3>Email: provostmat@gmail.com</h3>
-    </>
-  );
+  return <></>;
 }
