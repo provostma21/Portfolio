41b346c31d36ff5412d7abdb9db573037ec63ff3
Matthew M. Provost <provostmat@gmail.com>
2023-09-27 22:02:47 -0400

    added emailjs import and useRef import to Contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,4 +1,6 @@
 import React from "react";
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 import "../pages/Styles/Contact.css";
 
 export default function Contact() {
