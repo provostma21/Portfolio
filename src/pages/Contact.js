738a1f49691c1dfed3642fc5d9d19bad0648dae8
Matthew M. Provost <provostmat@gmail.com>
2023-10-02 21:50:48 -0400

    added emailjs Ids and key to .env file, then created constants for them on Contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,27 +2,24 @@ import React, { useRef } from "react";
 import emailjs from "@emailjs/browser";
 import "../pages/Styles/Contact.css";
 
+const serviceId = process.env.REACT_APP_SERVICE_ID;
+const templateId = process.env.REACT_APP_TEMPLATE_ID;
+const publicKey = process.env.REACT_APP_PUBLIC_KEY;
+
 const Contact = () => {
   const form = useRef();
 
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs
-      .sendForm(
-        "YOUR_SERVICE_ID",
-        "YOUR_TEMPLATE_ID",
-        form.current,
-        "YOUR_PUBLIC_KEY"
-      )
-      .then(
-        (result) => {
-          console.log(result.text);
-        },
-        (error) => {
-          console.log(error.text);
-        }
-      );
+    emailjs.sendForm(serviceId, templateId, form.current, publicKey).then(
+      (result) => {
+        console.log(result.text);
+      },
+      (error) => {
+        console.log(error.text);
+      }
+    );
   };
 
   return (
