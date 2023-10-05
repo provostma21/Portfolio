e8a611e6b7e38724e93ab2d03b712ba3ad1dcea4
Matthew M. Provost <provostmat@gmail.com>
2023-10-04 22:41:32 -0400

    added footer-bar className to div under container

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,9 @@
 import "./Style/Footer.css";
 
 export default function Footer() {
-  return <div className="footer-container"></div>;
+  return (
+    <div className="footer-container">
+      <div className="footer-bar"></div>
+    </div>
+  );
 }
