c16805cf1424d54e4aaf5968bad039cecf4014b1
Matthew M. Provost <provostmat@gmail.com>
2023-10-05 21:27:43 -0400

    added link to Email Icon on Footer component to open separate tab with email already placed in to field

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,6 +2,8 @@ import "./Style/Footer.css";
 import { Icon } from "@iconify/react";
 
 export default function Footer() {
+  const emailAddress = "provostmat@gmail.com";
+
   return (
     <div className="footer-container">
       <div className="footer-bar">
@@ -56,17 +58,23 @@ export default function Footer() {
           </li>
           <li>
             Email
-            <Icon
-              icon="line-md:email-twotone"
-              style={{
-                height: "40px",
-                width: "40px",
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-                margin: "auto",
-              }}
-            />
+            <a
+              href={`mailto:${emailAddress}`}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Icon
+                icon="line-md:email-twotone"
+                style={{
+                  height: "40px",
+                  width: "40px",
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  margin: "auto",
+                }}
+              />
+            </a>
           </li>
         </ul>
       </div>
