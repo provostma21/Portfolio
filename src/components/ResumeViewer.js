63a343a47c14d3dc06064daa94196f7728e51a3b
Matthew M. Provost <provostmat@gmail.com>
2023-10-06 20:58:37 -0400

    added code to display resume as in a preview window via a component

--- src/components/ResumeViewer.js
+++ src/components/ResumeViewer.js
@@ -1,2 +1,14 @@
 import React from "react";
 import { Document, Page } from "react-pdf";
+
+const ResumeViewer = ({ ResumePDF }) => {
+  return (
+    <div className="resume-viewer">
+      <Document file={ResumePDF}>
+        <Page pageNumber={1} />
+      </Document>
+    </div>
+  );
+};
+
+export default ResumeViewer;
